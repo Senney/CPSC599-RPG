e63c888e89afc15c2ad6c2eb0a202699f1aba1fe
ekpitout <ekpitout@zone44wc.cs.ucalgary.ca>
2014-02-19 14:08:51 -0700

    inventory ready for review

--- src/cpsc599/items/Inventory.java
+++ src/cpsc599/items/Inventory.java
@@ -8,6 +8,9 @@ public class Inventory {
 	ArrayList<Item> equip;
 	ArrayList<Item> carry;
 	
+	public static final int CARRY_SIZE = 3;
+	public static final int EQUIP_SIZE = 5;
+	
 	public static final int HEAD_SLOT = 0;
 	public static final int CHEST_SLOT = 1;
 	public static final int RHAND_SLOT = 2;
@@ -17,7 +20,39 @@ public class Inventory {
 	public Inventory()
 	{
 		Logger.debug("Inventory::contruc - Creating inventory.");
-		equip = new ArrayList<Item>();
-		carry = new ArrayList<Item>();
+		equip = new ArrayList<Item>(EQUIP_SIZE);
+		carry = new ArrayList<Item>(CARRY_SIZE);
+	}
+	
+	public boolean pickUp(Item item)
+	{
+		if(carry.get(CARRY_SIZE) != null)
+		{
+			Logger.debug("Inventory::pickUp - pickup failed, Inventory full");
+			return false;
+		}
+		carry.add(item);
+		Logger.debug("Inventory::pickUp - you just picked up a " + item.name);
+		return true;
+	}
+	
+	public boolean equip(Item item)
+	{
+		if(equip.get(item.equipSlot) == null)
+		{
+			Logger.debug("Inventory::equip - equip failed, you have something there already");
+			return false;
+		}
+		equip.add(item.equipSlot, item);
+		carry.remove(item);
+		Logger.debug("Inventory::pickUp - you just equipped a " + item.name);
+		return true;
+	}
+	
+	public boolean use(Item item)
+	{
+		carry.remove(item);
+		Logger.debug("Inventory::pickUp - you just used a " + item.name);
+		return true;
 	}
 }
