47d3865ab814c120bd5b24069d24a8b46748c828
Sean Heintz <sean.heintz@gmail.com>
2014-02-19 10:51:23 -0700

    Updated controls and integrated.

--- src/cpsc599/states/IntroLevelState.java
+++ src/cpsc599/states/IntroLevelState.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import cpsc599.OrbGame;
 import cpsc599.managers.LevelManager;
+import cpsc599.util.Controls;
 import cpsc599.util.Logger;
 
 /**
@@ -29,17 +30,17 @@ public class IntroLevelState extends LevelState {
     public void tick(Input input) {
         time++;
 
-        if (input.isKeyPressed(Input.Keys.UP)) {
+        if (input.isKeyPressed(Controls.UP)) {
             super.camera.translate(0f, -TRANSLATE_SPEED);
         }
 
-        if (input.isKeyPressed(Input.Keys.S)) {
+        if (input.isKeyPressed(Controls.DOWN)) {
             super.camera.translate(0f, TRANSLATE_SPEED);
         }
 
-        if (input.isKeyPressed(Input.Keys.A)) {
+        if (input.isKeyPressed(Controls.LEFT)) {
             super.camera.translate(TRANSLATE_SPEED, 0f);
-        } else if (input.isKeyPressed(Input.Keys.D)) {
+        } else if (input.isKeyPressed(Controls.RIGHT)) {
             super.camera.translate(-TRANSLATE_SPEED, 0f);
         }
 
--- src/cpsc599/util/Controls.java
+++ src/cpsc599/util/Controls.java
@@ -1,7 +1,29 @@
 package cpsc599.util;
 
+import com.badlogic.gdx.Input;
+
 /**
- * Created by srheintz on 19/02/14.
+ * Static variables related to various control elements for the game.
  */
 public class Controls {
+
+    /**
+     * Directional controls.
+     */
+    public static int UP = Input.Keys.UP;
+    public static int LEFT = Input.Keys.LEFT;
+    public static int RIGHT = Input.Keys.RIGHT;
+    public static int DOWN = Input.Keys.DOWN;
+
+    /**
+     * Activation buttons. NES had 2 buttons, so do we.
+     */
+    public static int A_BUTTON = Input.Keys.Z;
+    public static int B_BUTTON = Input.Keys.X;
+
+    /**
+     * Interaction buttons.
+     */
+    public static int START = Input.Keys.ENTER;
+    public static int SELECT = Input.Keys.SPACE;
 }
