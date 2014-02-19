2826e6eefe8eaa51dda46add053dba2bf8f50e34
Sean Heintz <sean.heintz@gmail.com>
2014-02-19 11:43:29 -0700

    Added basic collision detection.

--- src/cpsc599/assets/Level.java
+++ src/cpsc599/assets/Level.java
@@ -1,7 +1,10 @@
 package cpsc599.assets;
 
 import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import cpsc599.util.Logger;
 
 import java.io.File;
 
@@ -10,5 +13,17 @@ public class Level {
     public String name;
 
     public TiledMap tiledMap;
-    public MapLayer collisionLayer;
+    public TiledMapTileLayer collisionLayer;
+
+    public boolean collide(int x, int y) {
+        if (collisionLayer == null) {
+            Logger.error("Level::collide - Level has not yet been loaded, or collision layer was not assigned.");
+            return false;
+        }
+
+        boolean collides = collisionLayer.getCell(x, y) != null;
+        Logger.debug("Checking collision at (" + x + ", " + y + "): " + collides);
+
+        return collides;
+    }
 }
--- src/cpsc599/loaders/LevelLoader.java
+++ src/cpsc599/loaders/LevelLoader.java
@@ -3,6 +3,8 @@ package cpsc599.loaders;
 import com.badlogic.gdx.assets.AssetManager;
 
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import cpsc599.assets.Level;
 import cpsc599.util.Logger;
@@ -28,12 +30,12 @@ public class LevelLoader {
             Logger.fatal("LevelLoader::load - Unable to load requested Tiled Map: " + mapName);
             return false;
         }
-        if (level.tiledMap.getLayers().get("Collision") == null ||
-                level.tiledMap.getLayers().get("collision") == null) {
+
+        if (level.tiledMap.getLayers().get("Collision") == null) {
             Logger.fatal("LevelLoader::loadMap - The map *must* have a layer named 'Collision'.");
             return false;
         }
-        level.collisionLayer = level.tiledMap.getLayers().get("Collision");
+        level.collisionLayer = (TiledMapTileLayer)level.tiledMap.getLayers().get("Collision");
 
 		return true;
 	}
