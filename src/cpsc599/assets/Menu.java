aa93571ef859119d5268b172c5d0a3e6eef34bec
Sean Heintz <sean.heintz@gmail.com>
2014-02-28 12:15:11 -0700

    Created a menu package

--- src/cpsc599/assets/Menu.java
+++
@@ -1,7 +0,0 @@
-package cpsc599.assets;
-
-/**
- * Created by srheintz on 28/02/14.
- */
-public class Menu {
-}
---
+++ src/cpsc599/menus/Menu.java
@@ -0,0 +1,22 @@
+package cpsc599.menus;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/** Class for containing the information required to display a menu. **/
+public class Menu {
+    private int height, width;
+
+    public Menu(int width, int height) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public void render(SpriteBatch batch) {
+
+    }
+
+    public void tick(Input input) {
+
+    }
+}
