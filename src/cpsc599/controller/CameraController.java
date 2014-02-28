615270731644363ef197f1eed75a8b5f6e337a32
Sean Heintz <sean.heintz@gmail.com>
2014-02-28 09:51:28 -0700

    Filled out CameraController.

--- src/cpsc599/controller/CameraController.java
+++ src/cpsc599/controller/CameraController.java
@@ -1,7 +1,59 @@
 package cpsc599.controller;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import cpsc599.util.CoordinateTranslator;
+
 /**
- * Created by srheintz on 28/02/14.
+ * Class for controlling movement and positioning of the Camera.
  */
 public class CameraController {
+    private int x, y;
+    private double scale;
+    private int width, height;
+
+    private OrthographicCamera camera;
+
+    /**
+     * Creates the CameraController with the specified position and size.
+     * @param position (x, y) position on the screen specified in units related to the map (x * 16, y * 16)
+     * @param size (w, h, scale) the parameters that specify the size the camera should view.
+     */
+    public CameraController(Vector2 position, Vector3 size) {
+        this.x = CoordinateTranslator.translate((int)position.x);
+        this.y = CoordinateTranslator.translate((int)position.y);
+        this.width = (int)size.x;
+        this.height = (int)size.y;
+        this.scale = (double)size.z;
+
+        this.camera = new OrthographicCamera(size.x, size.y);
+    }
+
+    /**
+     * Sets the camera position in game-units.
+     * @param x the x position.
+     * @param y the y position.
+     */
+    public void set(int x, int y) {
+        // Check that the X and Y positions don't overflow the camera off of the map.
+        int realx = bound(CoordinateTranslator.translate(x), width);
+        int realy = bound(CoordinateTranslator.translate(y), width);
+
+        this.x = realx;
+        this.y = realy;
+
+        this.updateCamera();
+    }
+
+    private int bound(int x, int v) {
+        if (x - (v / 2) < 0) x = v / 2;
+        return x;
+    }
+
+    private void updateCamera() {
+        this.camera.position.x = this.x;
+        this.camera.position.y = this.y;
+        this.camera.update();
+    }
 }
