2101fb8ac98798c622598dd911fa0ee2b68db7c4
Sean Heintz <sean.heintz@gmail.com>
2014-04-02 10:58:14 -0600

    Fixed a variety of bugs in BasicWarrior ai

--- src/cpsc599/ai/BasicWarrior.java
+++ src/cpsc599/ai/BasicWarrior.java
@@ -15,23 +15,21 @@ public class BasicWarrior extends AIActor {
 
     @Override
     public boolean step(float time) {
-        if (actionList.size() == 0) return true;
-
         if (time >= nextStep) {
             AIAction action = actionList.get(0);
             if (action.action == AIAction.MOVE) {
                 List<AStarMove> movementList = (List<AStarMove>)action.obj;
-                if (movementList.size() == 0) {
+                if (movementList.size() == 1) {
                     actionList.remove(action);
-                    return false;
+                } else {
+                    AStarMove top = movementList.get(movementList.size() - 1);
+
+                    Logger.debug("Moving actor " + this.actor + " in direction (" + top.x_move + ", " + top.y_move + ")");
+                    this.actor.move(top.x_move, top.y_move, this.pathfinder.getLevel());
+
+                    movementList.remove(top);
+                    action.obj = movementList;
                 }
-                AStarMove top = movementList.get(movementList.size() - 1);
-
-                Logger.debug("Moving actor " + this.actor + " in direction (" + top.x_move + ", " + top.y_move + ")");
-                this.actor.move(top.x_move, top.y_move, this.pathfinder.getLevel());
-
-                movementList.remove(top);
-                action.obj = movementList;
             } else if (action.action == AIAction.ATTACK) {
                 Vector2 position = (Vector2)action.obj;
                 this.actor.attack(playerManager.getNearest(position));
@@ -40,13 +38,21 @@ public class BasicWarrior extends AIActor {
 
             nextStep += STEP_TIME;
         }
+        if (actionList.size() == 0) return true;
 
         return false;
     }
 
     @Override
     public void moveTo(int x, int y) {
+        Vector2 start = new Vector2(this.actor.x, this.actor.y),
+                end = new Vector2(x, y);
+        List<AStarMove> movements = pathfinder.getPath(start, end);
+        if (movements == null) {
+            return;
+        }
 
+        actionList.add(new AIAction(AIAction.MOVE, movements));
     }
 
     @Override
@@ -66,11 +72,12 @@ public class BasicWarrior extends AIActor {
     public boolean decideTurn() {
         Logger.debug("Deciding turn for BasicWarrior: " + this.actor);
 
+        this.nextStep = 0f;
         Vector2 position = new Vector2(this.actor.x, this.actor.y);
         Player nearest = playerManager.getNearest(position);
 
         // Move to the nearest player.
-        this.attackTo(nearest.x, nearest.y);
+        this.moveTo(nearest.x, nearest.y);
 
         return true;
     }
