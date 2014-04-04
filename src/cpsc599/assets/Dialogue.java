e68d3034f17e00920da89bc6397f35e3160d5187
Sean Heintz <sean.heintz@gmail.com>
2014-04-04 13:24:01 -0600

    Refactored Dialogue to be more dynamic

--- src/cpsc599/assets/Dialogue.java
+++ src/cpsc599/assets/Dialogue.java
@@ -8,6 +8,8 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+
+import cpsc599.util.SharedAssets;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
@@ -25,40 +27,71 @@ public class Dialogue {
 	private CharSequence textRemains;
 	private boolean visible;
 	private boolean textLeft = false;
+
+    private Document doc;
+
+    private float displayTime;
 	
 	public Dialogue() {
 		Logger.debug("Setting up text");
 		
-		boxHeight = Gdx.graphics.getHeight() - 325;
-		boxWidth = Gdx.graphics.getWidth() - 20;	// Adds 10px padding on left/right
-		
-		try {
-			Logger.debug("XML Parsing");
-			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = docFactory.newDocumentBuilder();
-			Document doc = db.parse(new File("src/cpsc599/assets/Script.xml"));
-			NodeList list = doc.getElementsByTagName("testing");
-			Node node = list.item(0);
-			//Logger.debug("Current Element :" + node.getNodeName());
-			Element elem = (Element) node;
-			text = elem.getTextContent();
-		}
-		catch(Exception e){
-			e.printStackTrace();
-			Logger.debug("Failed to parse XML");
-		}
+		boxHeight = (int)round16(Gdx.graphics.getHeight() - 325);
+		boxWidth = (int)round16(Gdx.graphics.getWidth() - 20);	// Adds 10px padding on left/right
 		
 		font = new BitmapFont();
 		
 		font.setScale((float) 1.1);
 		lineWidth = (int) (boxWidth/1.55);
-		
-		loadDialogue(text);
-		
+
+        displayTime = 0.f;
+
 		this.visible = false;
 	}
+
+    public boolean loadDialogueXML(String xmlFile) {
+        if (doc == null) {
+            try {
+                Logger.debug("Parsing XML from " + xmlFile);
+                DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = docFactory.newDocumentBuilder();
+                doc = db.parse(new File(xmlFile));
+            }
+            catch(Exception e){
+                e.printStackTrace();
+                Logger.debug("Failed to parse XML");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void showDialogueTime(float curTime, float showTime) {
+        if (!this.visible) {
+            this.displayTime = curTime + showTime;
+            this.visible = true;
+        }
+
+        if (curTime > this.displayTime) {
+            this.visible = false;
+        }
+    }
+
+    public void setDialogueText(String text) {
+        this.text = text;
+        loadDialogue();
+    }
+
+    public void setDialogueTag(String tagName) {
+        NodeList list = doc.getElementsByTagName(tagName);
+        Node node = list.item(0);
+        Element elem = (Element) node;
+        text = elem.getTextContent();
+
+        loadDialogue();
+    }
 	
-	public void loadDialogue(CharSequence dialogue) {
+	public void loadDialogue() {
 		String check;
 		int bounds = text.length();
 		int strEnd = 168;
@@ -79,11 +112,13 @@ public class Dialogue {
 		}
 		else
 			textLeft = false;
+
+        this.boxHeight = (int)round16(font.getWrappedBounds(this.text, lineWidth).height) + 32;
 	}
 	
 	public void loadTextRemains() {
 		text = textRemains;
-		loadDialogue(text);
+        loadDialogue();
 	}
 	
 	public boolean checkTextLeft() {
@@ -102,17 +137,50 @@ public class Dialogue {
 	
 	public void render(SpriteBatch batch) {
 		if (!visible) return;
-		
-		// Background of dialogue
-		ShapeRenderer shapeRenderer = new ShapeRenderer();
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.setColor(Color.BLUE);
-        shapeRenderer.rect(10, 10, boxWidth, boxHeight);
-        shapeRenderer.end();
 
         batch.begin();
+        drawTextBackdrop(batch, 10, 10, boxWidth, boxHeight);
         font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
-		font.drawWrapped(batch, text, 150, 120, lineWidth);
+        BitmapFont.TextBounds bounds = font.getWrappedBounds(this.text, this.lineWidth);
+		font.drawWrapped(batch, text, 150, 32 + bounds.height, lineWidth);
         batch.end();
 	}
+
+    private float round16(float value) {
+        if (value % 16 != 0) {
+            int numDiv = (int)(value / 16.f);
+            value = (numDiv+1) * 16;
+        }
+
+        return value;
+    }
+
+    private void drawTextBackdrop(SpriteBatch batch, int xpos, int ypos, int width, int height) {
+        if (width % 16 != 0 || height % 16 != 0) {
+            Logger.fatal("Unable to draw menu with non-16-divisible width and height.");
+            return;
+        }
+
+        int width_iter = width / 16;
+        int height_iter = height / 16;
+
+        // Draw the bottom and top
+        batch.draw(SharedAssets.menu_texture[2][0], xpos, ypos);
+        batch.draw(SharedAssets.menu_texture[0][0], xpos, ypos + (16 * (height_iter-1)));
+        for (int i = 1; i < width_iter - 1; i++) {
+            batch.draw(SharedAssets.menu_texture[2][1], xpos+(16*i), ypos);
+            batch.draw(SharedAssets.menu_texture[0][1], xpos+(16*i), ypos + (16 * (height_iter-1)));
+        }
+        batch.draw(SharedAssets.menu_texture[2][2], xpos + (16 * (width_iter-1)), ypos);
+        batch.draw(SharedAssets.menu_texture[0][2], xpos + (16 * (width_iter-1)), ypos + (16 * (height_iter-1)));
+
+        // Draw the middle
+        for (int i = 1; i < height_iter - 1; i++) {
+            batch.draw(SharedAssets.menu_texture[1][0], xpos, ypos + (16 *i));
+            for (int j = 1; j < width_iter - 1; j++) {
+                batch.draw(SharedAssets.menu_texture[1][1], xpos + (16*j), ypos + (16*i));
+            }
+            batch.draw(SharedAssets.menu_texture[1][2], xpos + (16 * (width_iter - 1)), ypos + (16*i));
+        }
+    }
 }
