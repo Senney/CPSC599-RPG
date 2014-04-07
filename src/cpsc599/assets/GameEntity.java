37e308e1ac3d840237e3042f600b7349ceaa4c32
Sean Heintz <sean.heintz@gmail.com>
2014-04-07 09:23:24 -0600

    Extended the GameEntity class a bit.

--- src/cpsc599/assets/GameEntity.java
+++ src/cpsc599/assets/GameEntity.java
@@ -1,19 +1,40 @@
 package cpsc599.assets;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * Class for representing interactive and dynamic entities within the game.
  */
 public abstract class GameEntity {
-    public GameEntity() {
+    protected String identifier;
+    protected String inspect;
 
+    private Sprite objSprite;
+    protected Vector2 position;
+
+    public GameEntity() {
+        this.identifier = "";
+        this.inspect = "";
+        this.objSprite = null;
+        this.position = new Vector2(-1, -1);
     }
 
     public abstract boolean tick(float time);
     public abstract void render(SpriteBatch batch);
     public abstract void onUse();
-    public abstract String onInspect();
+
+    public Vector2 getPosition() { return this.position; }
+    public void setPosition(Vector2 pos) { this.position = pos; }
+
+    public String onInspect() {
+        return inspect;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
 
     public boolean collides() {
         return false;
