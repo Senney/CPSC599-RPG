d23f8ab0bc1ecf45841b9223330511ffc3fca04e
Sean Heintz <sean.heintz@gmail.com>
2014-04-09 12:02:44 -0600

    Added framework for Opportunist

--- src/cpsc599/ai/OpportunistAI.java
+++ src/cpsc599/ai/OpportunistAI.java
@@ -1,7 +1,15 @@
 package cpsc599.ai;
 
-/**
- * Created by srheintz on 09/04/14.
- */
-public class OpportunistAI {
+import cpsc599.assets.Actor;
+import cpsc599.managers.PlayerManager;
+
+public class OpportunistAI extends AIActor {
+    public OpportunistAI(PlayerManager playerManager, AStarPathfinder pathfinder, Actor actor) {
+        super(playerManager, pathfinder, actor);
+    }
+
+    @Override
+    public boolean decideTurn() {
+        return false;
+    }
 }
