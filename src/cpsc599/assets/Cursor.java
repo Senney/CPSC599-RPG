bcf2a04e352c6890410919658f008467da795faf
Sean Heintz <sean.heintz@gmail.com>
2014-04-09 14:36:53 -0600

    Updated Cursor to use the shared asset

--- src/cpsc599/assets/Cursor.java
+++ src/cpsc599/assets/Cursor.java
@@ -2,16 +2,17 @@ package cpsc599.assets;
 
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import cpsc599.OrbGame;
 import cpsc599.util.CoordinateTranslator;
 import cpsc599.util.Logger;
 
 public class Cursor {
-    private AnimatedSprite cursorSprite;
+    private TextureRegion cursorSprite;
 
     public int x, y;
 
-    public Cursor(AnimatedSprite sprite){
+    public Cursor(TextureRegion sprite){
         this.cursorSprite = sprite;
     }
 
@@ -20,7 +21,7 @@ public class Cursor {
     }
 
     public void render(SpriteBatch batch) {
-        cursorSprite.render(batch, CoordinateTranslator.translate(x), CoordinateTranslator.translate(y));
+        batch.draw(this.cursorSprite, CoordinateTranslator.translate(x), CoordinateTranslator.translate(y));
     }
 
     public void move(int direction){
