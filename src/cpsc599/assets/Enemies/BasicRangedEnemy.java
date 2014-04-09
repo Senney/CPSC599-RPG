8b322e610d78174c32909e2071bd0e9253387856
Etienne <et.pitout@gmail.com>
2014-04-09 15:08:13 -0600

    Added enemies

---
+++ src/cpsc599/assets/Enemies/BasicRangedEnemy.java
@@ -0,0 +1,22 @@
+package cpsc599.assets.Enemies;
+
+import cpsc599.assets.AnimatedSprite;
+import cpsc599.assets.Enemy;
+
+public class BasicRangedEnemy extends Enemy{
+    public BasicRangedEnemy(AnimatedSprite sprite, int x, int y) {
+        super(sprite, x, y, 4);
+
+        int baseDamage = 2;
+        super.maxHealth = 8;
+        super.strength = 2;
+        super.defence = 1;
+        super.hit = 90;
+        super.dodge = 50;
+        super.range = 2;
+
+        super.damage = baseDamage + super.strength;
+
+        super.heal(-1);
+    }
+}
---
+++ src/cpsc599/assets/Enemies/NimbleTheifEnemy.java
@@ -0,0 +1,22 @@
+package cpsc599.assets.Enemies;
+
+import cpsc599.assets.AnimatedSprite;
+import cpsc599.assets.Enemy;
+
+public class NimbleTheifEnemy extends Enemy{
+    public NimbleTheifEnemy(AnimatedSprite sprite, int x, int y) {
+        super(sprite, x, y, 7);
+
+        int baseDamage = 3;
+        super.maxHealth = 7;
+        super.strength = 1;
+        super.defence = 1;
+        super.hit = 130;
+        super.dodge = 70;
+        super.range = 1;
+
+        super.damage = baseDamage + super.strength;
+
+        super.heal(-1);
+    }
+}
