422ea7c59ad03eb1a86d5af40cf6858a78e2cf16
Sean Heintz <sean.heintz@gmail.com>
2014-04-08 19:46:18 -0600

    Fixed camera bug for level1

--- src/cpsc599/states/Level1/IntroLevelState.java
+++ src/cpsc599/states/Level1/IntroLevelState.java
@@ -337,6 +337,8 @@ public class IntroLevelState extends LevelState {
                 } else {
                     currentEnemy++;
                 }
+
+                return;
             }
         }
 
