b6dc965bb36f1e887660d56e747e5a7de25b728b
Sean Heintz <sean.heintz@gmail.com>
2014-04-09 21:37:44 -0600

    Added health game entity functionality

--- src/cpsc599/assets/Entities/HealthGameEntity.java
+++ src/cpsc599/assets/Entities/HealthGameEntity.java
@@ -1,7 +1,27 @@
 package cpsc599.assets.Entities;
 
-/**
- * Created by srheintz on 09/04/14.
- */
-public class HealthGameEntity {
+import cpsc599.assets.Actor;
+import cpsc599.assets.GameEntity;
+import cpsc599.states.State;
+
+public class HealthGameEntity extends GameEntity {
+    private final int hp;
+
+    public HealthGameEntity(int x, int y, int hp) {
+        this.setPosition(x, y);
+        this.hp = hp;
+    }
+
+    @Override
+    public boolean tick(float time, State gameState) {
+        return false;
+    }
+
+    @Override
+    public String onUse(State gameState, Actor activator) {
+        activator.heal(this.hp);
+        activator.maxHealth += this.hp;
+
+        return "Added " + this.hp + " health to the player's maximum health.";
+    }
 }
